6b4e4db8840ad60d6ac8d1ed59ff79710a7bb182
Timon <timosha@gmail.com>
2013-03-22 17:20:51 +0600

    remove php

--- php/macros.php
+++
@@ -1,9 +0,0 @@
-#
-# Interface versions exposed by PHP:
-# 
-%php_core_api @PHP_APIVER@
-%php_zend_api @PHP_ZENDVER@
-%php_pdo_api  @PHP_PDOVER@
-%php_version  @PHP_VERSION@
-
-%php_extdir %{_libdir}/php/modules
